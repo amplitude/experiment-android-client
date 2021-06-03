23d7219b7b3ddfc881cebfc76fdc7e5b68314386
Brian Giori <brian.giori@amplitude.com>
2021-06-02 19:56:55 -0700

    add helper is func to variant

--- sdk/src/main/java/com/amplitude/experiment/Variant.kt
+++ sdk/src/main/java/com/amplitude/experiment/Variant.kt
@@ -3,4 +3,22 @@ package com.amplitude.experiment
 data class Variant @JvmOverloads constructor(
     @JvmField val value: String? = null,
     @JvmField val payload: Any? = null,
-)
+) {
+
+    /**
+     * Useful for comparing a variant's value to a string in java.
+     *
+     * ```
+     * variant.is("on");
+     * ```
+     *
+     * is equivalent to
+     *
+     * ```
+     * "on".equals(variant.value);
+     * ```
+     *
+     * @param value The value to compare with the value of this variant.
+     */
+    fun `is`(value: String): Boolean = this.value == value
+}
