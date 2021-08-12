9c50caa1397e8fb5e942bb1bb6d26a8ecf60bc82
Brian Giori <brian.giori@amplitude.com>
2021-08-12 09:46:52 -0700

    remove unused imports

--- sdk/src/main/java/com/amplitude/experiment/analytics/ExperimentAnalyticsEvent.kt
+++ sdk/src/main/java/com/amplitude/experiment/analytics/ExperimentAnalyticsEvent.kt
@@ -1,9 +1,5 @@
 package com.amplitude.experiment.analytics
 
-import com.amplitude.experiment.ExperimentUser
-import com.amplitude.experiment.ExperimentClient
-import com.amplitude.experiment.ExperimentUserProvider
-
 /**
  * Analytics event for tracking events generated from the experiment SDK client.
  * These events are sent to the implementation provided by an
