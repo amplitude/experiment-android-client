d26c996da527cffa57c11b9b32029dc2a52f687a
Brian Giori <brian.giori@amplitude.com>
2021-12-16 13:38:32 -0800

    dont spam exposure events for core analytics provider

--- sdk/src/main/java/com/amplitude/experiment/CoreAnalyticsProvider.kt
+++ sdk/src/main/java/com/amplitude/experiment/CoreAnalyticsProvider.kt
@@ -9,7 +9,17 @@ internal class CoreAnalyticsProvider(
     private val analyticsConnector: AnalyticsConnector
 ): ExperimentAnalyticsProvider {
 
+    private val setProperties = mutableMapOf<String, String>()
+    private val unsetProperties = mutableSetOf<String>()
+
     override fun track(event: ExperimentAnalyticsEvent) {
+        val variant = event.variant.value ?: return
+        if (setProperties[event.key] == variant) {
+            return
+        } else {
+            setProperties[event.key] = variant
+            unsetProperties.remove(event.key)
+        }
         val eventProperties: Map<String, String> = event.properties
             .filterValues { it != null }
             .mapValues { it.value!! }
@@ -22,13 +32,17 @@ internal class CoreAnalyticsProvider(
     }
 
     override fun setUserProperty(event: ExperimentAnalyticsEvent) {
+        val variant = event.variant.value ?: return
+        if (setProperties[event.key] == variant) {
+            return
+        }
         analyticsConnector.logEvent(
             AnalyticsEvent(
                 "\$identify",
                 null,
                 mapOf(
                     "\$set" to mapOf(
-                        event.userProperty to event.variant
+                        event.userProperty to variant
                     )
                 )
             )
@@ -36,13 +50,19 @@ internal class CoreAnalyticsProvider(
     }
 
     override fun unsetUserProperty(event: ExperimentAnalyticsEvent) {
+        if (unsetProperties.contains(event.key)) {
+            return
+        } else {
+            unsetProperties.add(event.key)
+            setProperties.remove(event.key)
+        }
         analyticsConnector.logEvent(
             AnalyticsEvent(
                 "\$identify",
                 null,
                 mapOf(
                     "\$unset" to mapOf(
-                        event.userProperty to event.variant
+                        event.userProperty to "-"
                     )
                 )
             )
