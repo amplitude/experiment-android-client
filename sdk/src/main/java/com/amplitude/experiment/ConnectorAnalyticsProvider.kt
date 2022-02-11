251a58e326573c9f4c28ac777352f982ab46fca9
Brian Giori <brian.giori@amplitude.com>
2022-02-11 09:32:29 -0800

    simplify connector analytics provider

--- sdk/src/main/java/com/amplitude/experiment/ConnectorAnalyticsProvider.kt
+++ sdk/src/main/java/com/amplitude/experiment/ConnectorAnalyticsProvider.kt
@@ -4,30 +4,19 @@ import com.amplitude.analytics.connector.EventBridge
 import com.amplitude.analytics.connector.AnalyticsEvent
 import com.amplitude.experiment.analytics.ExperimentAnalyticsEvent
 import com.amplitude.experiment.analytics.ExperimentAnalyticsProvider
-import com.amplitude.experiment.analytics.ExposureEvent
 
 internal class ConnectorAnalyticsProvider(
     private val eventBridge: EventBridge
 ): ExperimentAnalyticsProvider {
 
     override fun track(event: ExperimentAnalyticsEvent) {
-        if (event !is ExposureEvent) {
-            return
-        }
-        val variant = if (event.source.isFallback()) {
-            null
-        } else {
-            event.variant.value
-        }
-        val eventName = "\$exposure"
-        val eventProperties = mapOf(
-            "flag_key" to event.key,
-            "variant" to variant
-        ).filterNull()
         eventBridge.logEvent(
             AnalyticsEvent(
-                eventType = eventName,
-                eventProperties = eventProperties
+                eventType = "\$exposure",
+                eventProperties = mapOf(
+                    "flag_key" to event.key,
+                    "variant" to event.variant.value
+                ).filterNull()
             )
         )
     }
@@ -36,7 +25,14 @@ internal class ConnectorAnalyticsProvider(
     }
 
     override fun unsetUserProperty(event: ExperimentAnalyticsEvent) {
-        track(event)
+        eventBridge.logEvent(
+            AnalyticsEvent(
+                eventType = "\$exposure",
+                eventProperties = mapOf(
+                    "flag_key" to event.key,
+                ).filterNull()
+            )
+        )
     }
 }
 
