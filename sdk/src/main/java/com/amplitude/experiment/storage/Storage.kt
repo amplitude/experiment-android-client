794acb0cc53f73050356ec34eb984afe1d25b322
Tim Yiu <tim.yiu@Tims-MacBook-Pro.local>
2023-09-05 15:39:30 -0700

    removed unused import

--- sdk/src/main/java/com/amplitude/experiment/storage/Storage.kt
+++ sdk/src/main/java/com/amplitude/experiment/storage/Storage.kt
@@ -1,7 +1,5 @@
 package com.amplitude.experiment.storage
 
-import com.amplitude.experiment.Variant
-
 internal interface Storage {
     suspend fun get(key: String): String?
     suspend fun put(key: String, value: String)
