ea962e48450ff2a6058a9cc225626989f7d40af4
Tim Yiu <tim.yiu@Tims-MacBook-Pro.local>
2023-09-22 14:31:03 -0700

    remove flag!=null after decodeFromString

--- sdk/src/main/java/com/amplitude/experiment/FlagApi.kt
+++ sdk/src/main/java/com/amplitude/experiment/FlagApi.kt
@@ -59,9 +59,7 @@ internal class SdkFlagApi(
                     (0 until jsonArray.length()).forEach {
                         val jsonString = jsonArray.getJSONObject(it).toString()
                         val flag = json.decodeFromString<EvaluationFlag>(jsonString)
-                        if (flag != null) {
-                            flags[flag.key] = flag
-                        }
+                        flags[flag.key] = flag
                     }
 
                     future.complete(flags)
