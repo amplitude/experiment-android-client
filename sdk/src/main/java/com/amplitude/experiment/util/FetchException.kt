b9c3ba97012f3dfe1280da0a337a0c13ee0117eb
tyiuhc <tim.yiu@amplitude.com>
2024-01-26 11:26:34 -0800

    make FetchException internal

--- sdk/src/main/java/com/amplitude/experiment/util/FetchException.kt
+++ sdk/src/main/java/com/amplitude/experiment/util/FetchException.kt
@@ -2,7 +2,7 @@ package com.amplitude.experiment.util
 
 import okio.IOException
 
-class FetchException internal constructor(
+internal class FetchException internal constructor(
     val statusCode: Int,
     message: String
 ) : IOException(message)
