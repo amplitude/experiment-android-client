60cd0574a55068a6f03d9f1c01a88b6bc1caefab
Kenneth Yeh <kenneth.yeh@amplitude.com>
2025-11-21 21:44:32 -0800

    chore: update release to publish to central portal

--- release.config.js
+++ release.config.js
@@ -41,7 +41,7 @@ module.exports = {
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "./gradlew sdk:publishReleasePublicationToSonatypeRepository",
+      "publishCmd": "./gradlew publishToMavenCentral",
     }],
   ],
 }
